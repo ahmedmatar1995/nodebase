6921503795a04d5a8a93b5cad7008dabb7bb2edd
ahmedmt <ahmedmt411@gmail.com>
2025-11-14 14:17:38 +0200

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
 
-function page() {
+import React from 'react'
+import prisma from '@/lib/db'
+
+async function page() {
+  const users = await prisma.user.findMany();
+  console.log(users);
   return (
     <div>
       <p>home page</p>
+
     </div>
   )
 }
